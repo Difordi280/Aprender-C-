e23ad29fee035ddf4f8dd16d346600d54e98c96b
dithedark <dieferurbano28@hotmail.com>
2026-09-10 14:07:09 -0500

    Dia 3 SQL

--- Aprender/Ciclo 2 - SQL Server Relacional/Semana 1 - Modelado y Restricciones/D3 - Acciones referenciales en cascada/Ejercicio.sql
+++ Aprender/Ciclo 2 - SQL Server Relacional/Semana 1 - Modelado y Restricciones/D3 - Acciones referenciales en cascada/Ejercicio.sql
@@ -1,9 +1,38 @@
 -- D3 - 8 ejercicios
 USE C2D3;
 -- 1. Muestra Clientes y Pedidos.
+
+SELECT * FROM Clientes ;
+
+SELECT * FROM Pedidos;
+
+
 -- 2. Cambia el ID 1 a 10 y comprueba ON UPDATE CASCADE.
+
+UPDATE Clientes SET IdCliente = 10 WHERE( IdCliente = 1 );
+
+
 -- 3. Borra el cliente 2 y comprueba ON DELETE CASCADE.
+SELECT COUNT(*) FROM Pedidos;
+
+
+DELETE from Clientes WHERE( IdCliente = 2 );
+
+
 -- 4. Cuenta los pedidos antes y después del borrado.
+
+SELECT COUNT(*) FROM Pedidos;
+
+ALTER TABLE Pedidos 
+DROP CONSTRAINT FK_Pedidos_Clientes;
+
+
+ALTER TABLE Pedidos
+ADD CONSTRAINT FK_Pedidos_Clientes 
+FOREIGN KEY (IdCliente) REFERENCES Clientes(IdCliente);
+
+
+
 -- 5. Crea una relación sin cascada.
 -- 6. Intenta borrar el padre de esa relación y explica el bloqueo.
 -- 7. Consulta sys.foreign_keys y sus acciones.
